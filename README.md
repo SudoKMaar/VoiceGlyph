c002714a3ae508d152c3b95fa3268dd4685b0082
SudoKMaar <abhi2004shek.kumar@gmail.com>
2023-06-29 16:13:05 +0530

    Voice Glyph

---
+++ README.md
@@ -0,0 +1,96 @@
+## Voice Glyph
+
+Deployed at : https://voiceglyph.netlify.com/
+
+Voice Glyph is a revolutionary React web app that converts speech to text in real-time. It provides a seamless and intuitive experience for users to transcribe their spoken words into written text instantly. With Voice Glyph, you can effortlessly capture your ideas, take notes, or dictate content without the need for manual typing.
+
+## Features
+
+- Real-time speech-to-text conversion
+- Interactive and user-friendly interface
+- Effortlessly transcribe spoken words into written text
+- Copy transcribed text to the clipboard
+- Export transcribed text as a text file
+- Clear and reset the transcribed text
+
+## Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/SudoKMaar/voice-glyph.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```shell
+   cd voice-glyph
+   ```
+
+3. Install the dependencies:
+
+   ```shell
+   npm install
+   ```
+
+## Usage
+
+1. Start the development server:
+
+   ```shell
+   npm start
+   ```
+
+2. Open your web browser and visit `http://localhost:3000` to access the Voice Glyph app.
+
+## How It Works
+
+1. Click the "Start Listening" button to begin speech recognition.
+2. Speak into your device's microphone, and your speech will be converted to text in real-time.
+3. The transcribed text will be displayed in the main content area of the app.
+4. To copy the transcribed text to the clipboard, click the "Copy to clipboard" button.
+5. To export the transcribed text as a text file, click the "Export" button. The file will be downloaded as "transcript.txt".
+6. To clear the transcribed text and start afresh, click the "Clear" button.
+7. When finished, click the "Stop Listening" button to end speech recognition.
+
+## Folder Structure
+
+The project structure is organized as follows:
+
+```
+voice-glyph/
+  ├── src/
+  │   ├── vg_logo.jpg
+  │   ├── logo.svg
+  │   ├── index.js
+  │   ├── index.css
+  │   ├── bg.svg
+  │   ├── App.js
+  │   └── App.css
+  ├── public/
+  │   ├── robots.txt
+  │   ├── manifest.json
+  │   ├── logo521.jpg
+  │   ├── logo192.jpg
+  │   ├── index.html
+  │   └── favicon.ico
+  ├── package.json
+  ├── package-lock.json
+  └── README.md
+```
+
+- `src/`: Contains the source code of the Voice Glyph app.
+- `src/components/`: Contains individual React components used in the app.
+- `src/logo.jpg`: Logo image file used in the app.
+- `public/`: Contains the public assets of the app, such as HTML and favicon.
+- `package.json`: Defines the project dependencies and scripts.
+- `README.md`: Provides information about the Voice Glyph app and how to use it.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or would like to enhance the Voice Glyph app, feel free to open a pull request.
+
+## License
+
+This Voice Glyph app is open source and available under the [MIT License](LICENSE).
+```
---
+++ src/App.js
@@ -0,0 +1,92 @@
+import "./App.css";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { useState } from "react";
+import logo from "./vg_logo.jpg";
+import { saveAs } from "file-saver";
+
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState("");
+  const [isCopied, setCopied] = useState(false);
+  const [showContent, setShowContent] = useState(true);
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  const currentYear = new Date().getFullYear();
+
+  const handleExport = () => {
+    const blob = new Blob([transcript], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "transcript.txt");
+  };
+
+  const handleClear = () => {
+    setTextToCopy("");
+    resetTranscript();
+  };
+
+  const handleCopyToClipboard = () => {
+    if (transcript) {
+      navigator.clipboard
+        .writeText(transcript)
+        .then(() => {
+          setCopied(true);
+          setTimeout(() => {
+            setCopied(false);
+          }, 1000);
+        })
+        .catch((error) => {
+          console.error("Failed to copy text to clipboard:", error);
+        });
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="column">
+          <img src={logo} alt="Logo" className="logo" />
+          <h2>Voice Glyph</h2>
+        </div>
+        <p>
+          Introducing Voice Glyph: A revolutionary React web app for real-time
+          speech-to-text conversion. Effortlessly transcribe spoken words into
+          written text, capturing ideas and taking notes with ease. Experience
+          the convenience and speed of Voice Glyph today...
+        </p>
+
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {showContent ? transcript : null}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={handleCopyToClipboard}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+          <button onClick={handleExport}>Export</button>
+          <button onClick={handleClear}>Clear</button>
+        </div>
+
+        <footer className="footer">
+          &copy; {currentYear} Abhishek Kumar. All rights reserved. | Powered by KMaar Miscellaneous Studio
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
+// Introducing Voice Glyph: The Revolutionary React Web App that Converts Speech to Text in Real-Time! It provides a seamless and intuitive experience for users to transcribe their spoken words into written text instantly. With Voice Glyph, you can effortlessly capture your ideas, take notes, or dictate content without the need for manual typing.
+
+// Voice Glyph leverages the power of speech recognition technology to accurately convert spoken language into written text. Powered by React, this web application offers a modern and interactive interface that enhances the user experience. Whether you're a student, professional, or anyone looking for a convenient way to transcribe speech, Voice Glyph is the perfect tool for you.
+
+// Developed by Abhishek Kumar, Voice Glyph combines cutting-edge technology with user-friendly design. It empowers individuals to communicate effectively and efficiently by eliminating the barriers posed by traditional typing methods. With Voice Glyph, you can unleash the power of your voice and transform it into written words with ease.
+
+// Experience the convenience and speed of real-time speech-to-text conversion with Voice Glyph. Try it now and revolutionize the way you capture and document your thoughts.
